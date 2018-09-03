8056d01132606a82b20bd7b7e2ff1d3e2115d1ee
Rizwanul Haque <mrizwan701@gmail.com>
2018-09-03 12:47:30 +0530

    pipeline code push

---
+++ src/test/java/testPackage/Demo1.java
@@ -0,0 +1,19 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Demo1 {
+    WebDriver driver = new ChromeDriver();
+    @Test
+    public void method1() {
+
+        driver.get("https://facebook.com");
+    }
+    @Test
+    public void method2(){
+        driver.navigate().to("https://bing.com");
+        driver.close();
+    }
+}
