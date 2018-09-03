27a972bd9004dc6557266cf2baa8954446e3dcdf
Rizwanul Haque <mrizwan701@gmail.com>
2018-09-03 12:57:38 +0530

    some change

--- src/test/java/testPackage/Demo1.java
+++ src/test/java/testPackage/Demo1.java
@@ -8,12 +8,12 @@ public class Demo1 {
     WebDriver driver = new ChromeDriver();
     @Test
     public void method1() {
-
         driver.get("https://facebook.com");
     }
     @Test
     public void method2(){
         driver.navigate().to("https://bing.com");
         driver.close();
+        driver.quit();
     }
 }
